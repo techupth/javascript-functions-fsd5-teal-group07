f10b48c61503c42d1df8e5299e45db5eb400b540
mygit <vuttipat.rungrojbribun@gmail>
2023-11-29 14:03:49 +0700

    dear:assignment

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,18 @@
 // Start coding here
+// Start coding here
+// Start coding here
+let add = (a, b) => {
+  return a + b;
+};
+
+let subtract = (a, b) => {
+  return a - b;
+};
+
+let multiply = (a, b) => {
+  return a * b;
+};
+
+let divide = (a, b) => {
+  return a / b;
+};
--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,17 @@
 // Start coding here
-let calculator = {};
+const calculator = {
+  add: function add(a, b) {
+    return a + b;
+  },
+  subtract: function subtract() {},
+  multiply: function multiply() {},
+  divice: function divice(a, b) {
+    return a / b;
+  },
+};
+
+const addResult = calculator.add(10, 20);
+console.log(addResult);
+
+const divideResult = calculator.divice(3000, 10);
+console.log(divideResult);
